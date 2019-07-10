c549c964af7273a47c46ab7bc0ff8f7f4141a4e2
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-10 16:39:21 +0700

    component rating bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameCardComponent } from './name-card/name-card.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { RatingBarComponent } from './rating-bar/rating-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameCardComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    RatingBarComponent
   ],
   imports: [
     BrowserModule,
